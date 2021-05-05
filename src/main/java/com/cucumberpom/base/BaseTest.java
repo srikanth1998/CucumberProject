d6bd85793a5b00ca96599ed5a84443088a20c9ed
srikanth1998 <42240687+srikanth1998@users.noreply.github.com>
2021-05-05 10:11:53 +0530

    First commit

---
+++ src/main/java/com/cucumberpom/base/BaseTest.java
@@ -0,0 +1,45 @@
+package com.cucumberpom.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.cucumberpom.utils.Constants;
+
+
+public class BaseTest{
+
+	public static WebDriver driver;
+	public static Properties prop;
+
+	public BaseTest() {
+		prop = new Properties();
+		try {
+			FileInputStream fis = new FileInputStream(
+					System.getProperty("user.dir") + "\\src\\main\\java\\com\\cucumberpom\\config\\config.properties");
+			prop.load(fis);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void initBrowser() {
+		String browser = prop.getProperty("browser");
+
+		if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\welcome\\eclipse-workspace\\IssueModule\\Driver\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else {
+			System.out.println("Chorme was not defined in Configuration");
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Constants.IMPLICITLY_WAIT, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(Constants.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+	}
+
+}
